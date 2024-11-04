e44fdc574a1a25b5296518b3a473f16ba9aaf633
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-11-04 22:08:45 +0530

    new changes

--- tem3.py
+++ tem3.py
@@ -1,62 +1,64 @@
+# coding:utf-8
 import sys
-from PyQt5.QtWidgets import QFrame,QHBoxLayout,QApplication
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon 
-from qfluentwidgets import NavigationItemPosition, FluentWindow, SubtitleLabel, setFont
+
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtGui import QIcon, QDesktopServices
+from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout
+from qfluentwidgets import setTheme, Theme, SubtitleLabel, setFont, SplitFluentWindow
 from qfluentwidgets import FluentIcon as FIF
+from qframelesswindow.webengine import FramelessWebEngineView
+
 
 class Widget(QFrame):
 
-    def __init__(self, text: str, parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.label = SubtitleLabel(text, self)
-        self.hBoxLayout = QHBoxLayout(self)
+        self.setObjectName("homeInterface")
 
-        setFont(self.label, 24)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        self.webView = FramelessWebEngineView(self)
+        self.webView.load(QUrl("https://www.baidu.com/"))
 
-        # Must set a globally unique object name for the sub-interface
-        self.setObjectName(text.replace(' ', '-'))
+        self.vBoxLayout = QVBoxLayout(self)
+        self.vBoxLayout.setContentsMargins(0, 48, 0, 0)
+        self.vBoxLayout.addWidget(self.webView)
 
 
-class Window(FluentWindow):
-    """ Main Interface """
+class Window(SplitFluentWindow):
 
     def __init__(self):
         super().__init__()
 
-        # Create sub-interfaces, when actually using, replace Widget with your own sub-interface
-        self.homeInterface = Widget('Home Interface', self)
-        self.musicInterface = Widget('Music Interface', self)
-        self.videoInterface = Widget('Video Interface', self)
-        self.settingInterface = Widget('Setting Interface', self)
-        self.albumInterface = Widget('Album Interface', self)
-        self.albumInterface1 = Widget('Album Interface 1', self)
+        # create sub interface
+        self.homeInterface = Widget(self)
 
         self.initNavigation()
         self.initWindow()
 
     def initNavigation(self):
-        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
-        self.addSubInterface(self.musicInterface, FIF.MUSIC, 'Music library')
-        self.addSubInterface(self.videoInterface, FIF.VIDEO, 'Video library')
+        self.addSubInterface(self.homeInterface, FIF.HOME, "Home")
 
-        self.navigationInterface.addSeparator()
-
-        self.addSubInterface(self.albumInterface, FIF.ALBUM, 'Albums', NavigationItemPosition.SCROLL)
-        self.addSubInterface(self.albumInterface1, FIF.ALBUM, 'Album 1', parent=self.albumInterface)
-
-        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+        # NOTE: enable acrylic effect
+        # self.navigationInterface.setAcrylicEnabled(True)
 
     def initWindow(self):
         self.resize(900, 700)
         self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
         self.setWindowTitle('PyQt-Fluent-Widgets')
 
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+
 
 if __name__ == '__main__':
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    # setTheme(Theme.DARK)
+
     app = QApplication(sys.argv)
     w = Window()
     w.show()
-    app.exec()
+    w.setMicaEffectEnabled(True)
+    app.exec_()
