6f40eb659fc8d2e8f18f3689fd78c7a622c6d999
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-11-12 14:19:08 +0530

    start new project

--- qfluent_widget_practice.py
+++ qfluent_widget_practice.py
@@ -7,24 +7,22 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout
 from qfluentwidgets import PrimaryPushButton, LineEdit
 
-driver = webdriver.Chrome()
-driver.get("www.google.com")
+def automation():
+    driver = webdriver.Chrome()
+    driver.get(lineEdit)
 
 class Mainwindow(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
-        self.setStyleSheet("background: rgb(225,225,225);")  # बैकग्राउंड रंग सेट किया गया
+        self.setStyleSheet("background: rgb(225,225,225);")  
 
     def init_ui(self):
         self.resize(900, 600)
 
-        # Main layout for the window
         layout = QVBoxLayout(self)
-
         layout.addStretch(3)
-
-        # LineEdit
+        global lineEdit   
         lineEdit = LineEdit()
         lineEdit.setPlaceholderText("example@example.com")
         lineEdit.setText("shokokawaii@foxmail.com")
@@ -32,14 +30,12 @@ class Mainwindow(QWidget):
         lineEdit.setClearButtonEnabled(True)
         layout.addWidget(lineEdit,alignment= Qt.AlignCenter)
       
-        # Add stretch above the button to center it vertically
-
-        # Button
+     
         button = PrimaryPushButton("push button")
-        button.setFixedSize(150, 40)  # Optional: Set a fixed size for the button
+        button.setFixedSize(150, 40) 
         layout.addWidget(button, alignment=Qt.AlignCenter)
+        button.clicked.connect(automation)
 
-        # Add stretch below the button to center it vertically
         layout.addStretch(3)
 
 if __name__ == '__main__':
