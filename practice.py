98743c6173c6155ae2fcdf4fe96c6007038e1f30
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-11-14 14:46:53 +0530

    screenshot feachar add in my project

--- practice.py
+++ practice.py
@@ -10,49 +10,43 @@ class Acount:
               4.debit amount.
               """
               )
-
         while True:
             choise = int(input("Enter your choise:"))
             sum = 0  
-
+            
             # try:
             #     for num in total:
             #         sum + num
             #         print(f"The total balance is {sum}")
             # except Exception as e:
             #     print("an error...")
-            
             if choise == 1:
                 print(f"Your total balance is {self.balance}")
-
             elif choise == 2:
                 print(f"your account no.{account_no}")
-
             elif choise == 3:
                 amo = int(input("Enter your amount which you want to credit:"))
-                
                 if amo > 0:
                     self.balance += amo
 
                 # amount = amo
                 # total = self.balance + amount
-                print(f"Total balance is = {self.balance}: ")
 
+                print(f"Total balance is = {self.balance}: ")
             elif choise == 4:
                 amount = int(input("Enter amount wich you want to debit:"))
 
                 if amount > 0:
                     self.balance -= amount
-
                 print(f"Your total amount is = {self.balance}")
-
             else:
                 break
-                
+
             # def debit(self,amount2):
             #     self.amount2 = amount2
             #     self.total2 = self.total - self.amount2
             #     print(f"Total balance is = {self.total2}:")
+
 
 acc1  = Acount(10000, 112233445566)
 
--- qfluent_widget_practice.py
+++ qfluent_widget_practice.py
@@ -1,19 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Ec
 from time import sleep
 import sys
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QThread
 from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout
 from qfluentwidgets import PrimaryPushButton, LineEdit
 
-def get_data():
-    global link
-    link = lineEdit.get()
+class Automationworker(QThread):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
 
-def automation():
-    driver = webdriver.Chrome()
-    driver.get(link)
+    def run(self):
+        driver = webdriver.Chrome()
+        driver.get(self.url)
+        driver.maximize_window()
+        try:
+            google_search = WebDriverWait(driver,20).until(Ec.element_to_be_clickable((By.XPATH, '//*[@id="APjFqb"]')))
+            google_search.send_keys("w3school.com")
+            google_search.send_keys(Keys.ENTER)
+
+            w3schools_link = WebDriverWait(driver, 10).until(Ec.element_to_be_clickable((By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3')))
+            w3schools_link.click()
+        
+            screenshot_path = "screenshot.png"
+            driver.save_screenshot(screenshot_path)
+            print(f"screenshot saved in {screenshot_path}")    
+        except Exception as e:
+            print("An error occurred...")
+
+        sleep(30)
+        driver.quit()
 
 class Mainwindow(QWidget):
     def __init__(self):
@@ -25,22 +45,30 @@ class Mainwindow(QWidget):
         self.resize(900, 600)
 
         layout = QVBoxLayout(self)
-        layout.addStretch(3)
-        global lineEdit   
-        lineEdit = LineEdit()
-        lineEdit.setPlaceholderText("example@example.com")
-        lineEdit.setText("shokokawaii@foxmail.com")
-        print(lineEdit.text())
-        lineEdit.setClearButtonEnabled(True)
-        layout.addWidget(lineEdit,alignment= Qt.AlignCenter)
-      
-     
+        layout.addStretch(1) 
+        self.lineEdit = LineEdit()
+        self.lineEdit.setPlaceholderText("example@example.com")
+        self.lineEdit.setText("")
+        print(self.lineEdit.text())
+    
+        self.lineEdit.setClearButtonEnabled(True)
+        layout.addWidget(self.lineEdit,alignment= Qt.AlignCenter)
+
         button = PrimaryPushButton("push button")
         button.setFixedSize(150, 40) 
         layout.addWidget(button, alignment=Qt.AlignCenter)
-        button.clicked.connect(automation)
+        button.clicked.connect(self.automation)
 
         layout.addStretch(3)
+
+    def automation(self):
+        url = self.lineEdit.text()
+        if url:
+            self.worker = Automationworker(url)
+            self.worker.start()
+        else:
+            print("please enter a valid url....")
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
